999f502cd1768dbe63d00061ad199a7cf79a4e45
mushroomyyy <jordanvckj@gmail.com>
2023-10-03 16:15:54 +0800

    resolved safety issues and naming conventions

--- capstone/src/main/java/capstone/Views/TellerCreateAccountView.java
+++ capstone/src/main/java/capstone/Views/TellerCreateAccountView.java
@@ -3,8 +3,10 @@ package capstone.Views;
 import capstone.Enums.AccountType;
 import capstone.Extras.ConsoleColours;
 import capstone.Extras.Helper;
+import capstone.Objects.Account;
 import capstone.Objects.Customer;
 import capstone.Objects.Database;
+import java.util.Optional;
 
 public class TellerCreateAccountView extends View {
 
@@ -21,31 +23,35 @@ public class TellerCreateAccountView extends View {
         continue;
       }
       Customer customer = Database.getCustomer(username).get();
+      Optional<Account> acc = null;
+      while (true) {
 
-      System.out.println(
-          ConsoleColours.GREEN_BRIGHT
-              + "Select which type of Account you would like to create "
-              + ConsoleColours.RESET);
-      System.out.println("1: Current Account");
-      System.out.println("2: Savings Account");
-      String account_type = Helper.readLine();
-      ;
-
-      if (account_type.equals("1")) {
-        Database.createAccount(customer, AccountType.CURRENT).get();
-        break;
-      } else if (account_type.equals("2")) {
-        Database.createAccount(customer, AccountType.SAVINGS).get();
-        break;
-      } else {
         System.out.println(
-            ConsoleColours.RED + "Invalid input, Select either 1 or 2" + ConsoleColours.RESET);
+            ConsoleColours.GREEN_BRIGHT
+                + "Select which type of Account you would like to create "
+                + ConsoleColours.RESET);
+        System.out.println("1: Current Account");
+        System.out.println("2: Savings Account");
+        String account_type = Helper.readLine();
+
+        if (account_type.equals("1")) {
+          acc = Database.createAccount(customer, AccountType.CURRENT);
+          break;
+        } else if (account_type.equals("2")) {
+          acc = Database.createAccount(customer, AccountType.SAVINGS);
+          break;
+        } else {
+          System.out.println(
+              ConsoleColours.RED + "Invalid input, Select either 1 or 2" + ConsoleColours.RESET);
+        }
+      }
+
+      if (acc.isEmpty()) {
+        System.out.println("Something wrong");
       }
 
       int continue_checker = Helper.continue_checker();
       if (continue_checker == 1) {
-        break;
-      } else {
         continue;
       }
     }
